c094362292b1d56ba0e3d7beac3931296c4a018f
Dc Liao <dcliao92@gmail.com>
2016-11-12 16:02:18 -0500

    Venues page fetch data from api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import ActivityReducer from './reducer_activities';
 import RequestsReducer from './reducer_requests';
+import VenuesReducer from './reducer_venues';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   activities: ActivityReducer,
-  requests: RequestsReducer
+  requests: RequestsReducer,
+  venues: VenuesReducer
 });
 
 export default rootReducer;
