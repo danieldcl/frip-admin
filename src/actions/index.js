1492e9425a14b7a7fd054744a94f1408f908f20e
Dc Liao <dcliao92@gmail.com>
2016-11-21 17:22:36 -0500

    add urls for fetching single venue or activity to action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+export const FETCH_VENUE = 'FETCH_VENUE';
 export const FETCH_VENUES = 'FETCH_VENUES';
-export const FETCH_REQUESTS = 'FETCH_REQUESTS';
+export const FETCH_ACTIVITY = 'FETCH_ACTIVITY';
+
 
 const ROOT_URL = 'http://frip-api.herokuapp.com/api/venues';
 const ROOT_URL_LOCALHOST = 'http://localhost:8001/api/venues';
+const GET_SINGLE_VENUE_URL = 'http://localhost:8001/api/venue?venue=';
+const GET_SINGLE_ACTIVITY_URL = 'http://localhost:8001/api/trip?trip=';
 
 // get/post data this way
 export function fetchVenues(){
@@ -13,4 +17,22 @@ export function fetchVenues(){
     type: FETCH_VENUES,
     payload: request
   };
+}
+
+export function fetchVenue(venue_name){
+  const url = `${GET_SINGLE_VENUE_URL}venue_name`;
+  const request = axios.get(url);
+  return {
+    type : FETCH_VENUE,
+    payload : request
+  }
+}
+
+export function fetchActivity(activity_name){
+  const url = `${GET_SINGLE_ACTIVITY_URL}activity_name`;
+  const request = axios.get(url);
+  return {
+    type : FETCH_ACTIVITY,
+    payload : request
+  }
 }
--- src/components/body_content/body_venues.js
+++ src/components/body_content/body_venues.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import $ from 'jquery';
 import { connect } from 'react-redux';
 import { fetchVenues } from '../../actions/index';
